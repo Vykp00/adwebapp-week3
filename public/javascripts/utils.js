99791f354ee6ab73cfdbf6bd04c4c9249139368b
Viivi Kauppinen <hellothaovy@yahoo.com.vn>
2023-11-20 08:48:40 +0200

    switch from "$" jQuery to native Javascript cus stupid Code Grade

--- public/javascripts/utils.js
+++ public/javascripts/utils.js
@@ -1,96 +1,149 @@
-$(document).ready(function() {
-    $('#submit-data').click(function(e) {
-      e.preventDefault();
+// public/javascripts/app.js
 
-      //Get values from input fields
-      const name = $('#input-name').val();
-      const task = $('#input-task').val();
+document.addEventListener('DOMContentLoaded', function () {
+// Function to send a POST request
+function postRequest(url, data, callback) {
+    var xhr = new XMLHttpRequest();
+    xhr.open('POST', url, true);
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.onreadystatechange = function () {
+    if (xhr.readyState == 4 && xhr.status == 200) {
+        callback(xhr.responseText);
+    }
+    };
+    xhr.send(JSON.stringify(data));
+}
 
-      // Send a POST request to the server
-      $.post('/todo', { name, task }, function(response) {
+// Function to send a GET request
+function getRequest(url, callback, errorCallback) {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', url, true);
+    xhr.onreadystatechange = function () {
+    if (xhr.readyState == 4) {
+        if (xhr.status == 200) {
+        callback(JSON.parse(xhr.responseText));
+        } else {
+        errorCallback(xhr.responseText);
+        }
+    }
+    };
+    xhr.send();
+}
+
+// Function to send a DELETE request
+function deleteRequest(url, callback, errorCallback) {
+    var xhr = new XMLHttpRequest();
+    xhr.open('DELETE', url, true);
+    xhr.onreadystatechange = function () {
+    if (xhr.readyState == 4) {
+        if (xhr.status == 200) {
+        callback(xhr.responseText);
+        } else {
+        errorCallback(xhr.responseText);
+        }
+    }
+    };
+    xhr.send();
+}
+
+// Function to send a PUT request
+function putRequest(url, data, callback, errorCallback) {
+    var xhr = new XMLHttpRequest();
+    xhr.open('PUT', url, true);
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.onreadystatechange = function () {
+    if (xhr.readyState == 4) {
+        if (xhr.status == 200) {
+        callback(xhr.responseText);
+        } else {
+        errorCallback(xhr.responseText);
+        }
+    }
+    };
+    xhr.send(JSON.stringify(data));
+}
+
+// Event listener for submitting data
+document.getElementById('submit-data').addEventListener('click', function (e) {
+    e.preventDefault();
+    var name = document.getElementById('input-name').value;
+    var task = document.getElementById('input-task').value;
+
+    // Send a POST request to the server
+    postRequest('/todo', { name: name, task: task }, function (response) {
+        // Parse the JSON response
+        var jsonResponse = JSON.parse(response);
+    // Display the server response
+    document.getElementById('response-message').innerText = jsonResponse.response;
+    });
+});
+
+// Event listener for searching user
+document.getElementById('search').addEventListener('click', function (e) {
+    e.preventDefault();
+    var searchName = document.getElementById('search-name').value;
+
+    // Send a GET request to the server
+    getRequest(`/user/${searchName}`, function (data) {
+    // Display user details
+    var userDetails = document.getElementById('user-details');
+    userDetails.innerHTML = `<p>Name: ${data.name}</p><p>Todos:</p>`;
+
+    // Display todos and delete task buttons
+    var todosList = document.getElementById('todos-list');
+    todosList.innerHTML = '';
+    data.todos.forEach(function (todo, index) {
+        var listItem = document.createElement('li');
+        listItem.innerHTML = `${todo} <button class="delete-task" data-index="${index}">Delete</button>`;
+        todosList.appendChild(listItem);
+    });
+
+    // Show delete user button
+    document.getElementById('delete-user').style.display = 'block';
+    }, function (error) {
+    // Display error message if user not found
+    document.getElementById('search-results').innerText = error;
+
+    // Hide delete user button
+    document.getElementById('delete-user').style.display = 'none';
+    });
+});
+
+// Event listener for delete task button clicks
+document.getElementById('todos-list').addEventListener('click', function (e) {
+    if (e.target.classList.contains('delete-task')) {
+    var taskIndex = e.target.dataset.index;
+    var searchName = document.getElementById('search-name').value;
+
+    // Send a PUT request to delete the task
+    putRequest('/user', { name: searchName, taskIndex: taskIndex }, function (response) {
         // Display the server response
-        $('#response-message').html(`<p>${response.response}</p>`);
-        //alert(response);
-      });
+        document.getElementById('search-results').innerText = response;
+
+        // Refresh the todos list
+        document.getElementById('search').click();
+    }, function (error) {
+        // Display error message if task deletion fails
+        document.getElementById('search-results').innerText = error;
     });
+    }
+});
 
-    $('#search').click(function(e) {
-      e.preventDefault();
+// Event listener for delete user button
+document.getElementById('delete-user').addEventListener('click', function (e) {
+    e.preventDefault();
+    var searchName = document.getElementById('search-name').value;
 
-      // Get value from search input
-      const searchName = $('#search-name').val();
+    // Send a DELETE request to the server
+    deleteRequest(`/user/${searchName}`, function (response) {
+    // Display the server response
+    document.getElementById('search-results').innerText = response;
 
-      // Send a GET request to the server
-      $.get(`/user/${searchName}`, function(data) {
-        // Display user details
-        $('#user-details').html(`<p>Name: ${data.name}</p><p>Todos:</p>`);
-
-        // Display todos and delete task buttons
-        const todosList = $('#todos-list');
-        todosList.empty();
-
-        data.todos.forEach((todo, index) => {
-          const listItem = $(`<li>${todo} <button class="delete-task" data-index="${index}">Delete</button></li>`);
-          todosList.append(listItem);
-        });
-
-        // Show delete user button
-        $('#delete-user').show();       
-      }).fail(function(xhr) {
-        // Display error message if user not found
-        $('#search-results').text(xhr.responseText);
-
-        // Hide delete user button
-        $('#delete-user').hide();
-      });
+    // Hide delete user button
+    document.getElementById('delete-user').style.display = 'none';
+    }, function (error) {
+    // Display error message if user not found
+    document.getElementById('search-results').innerText = error;
     });
-
-    // Handle delete task button clicks
-    $('#todos-list').on('click', '.delete-task', function () {
-      const taskIndex = $(this).data('index');
-      const searchName = $('#search-name').val();
-
-      // Send a PUT request to delete the task
-      $.ajax({
-        // Update the URL to use the correct path
-        url: `/user`,
-        type: 'PUT',
-        data: { name: searchName, taskIndex: taskIndex },
-        success: function (response) {
-          // Display the server response
-          $('#search-results').text(response);
-
-          // Refresh the todos list
-          $('#search').click();
-        },
-        error: function (xhr) {
-          // Display error message if task deletion fails
-          $('#search-results').text(xhr.responseText);
-        }
-      });
-    });
-
-    $('#delete-user').click(function(e) {
-      e.preventDefault();
-
-      // Get value from search input
-      const searchName = $('#search-name').val();
-
-      // Send a DELETE request to the server
-      $.ajax({
-        url: `/user/${searchName}`,
-        type: 'DELETE',
-        success: function(response) {
-          // Display the server response
-          $('#search-results').text(response);
-
-          // Hide delete user button
-          $('#delete-user').hide();
-        },
-        error: function(xhr) {
-          // Display error message if user not found
-          $('#search-results').text(xhr.responseText);
-        }
-      });
-    });
-  })
+});
+});
